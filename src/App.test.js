b295a21f22658300006f541a2f23eb07bee793e2
david lavieri <dvlavieri@gmail.com>
2020-01-27 10:27:13 -0800

    add connected render test for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { createStore } from 'redux';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import rootReducer from './redux/reducers'
+
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+}
+
+const setup = (initialState={}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<App store={store}/>).dive().dive();
+  return wrapper;
+}
+
+test('App renders without crashing', () => {
+  const wrapper = setup();
+  const appDiv = wrapper.find({"className": "app-router-div"});
+  expect(appDiv.length).toBe(1);
+})
